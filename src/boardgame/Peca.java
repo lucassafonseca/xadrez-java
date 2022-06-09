1a0180403fb7957244c45461fc5190d99fe2fd3a
lucassafonseca <lucassafonseca@gmail.com>
2022-06-09 20:55:03 -0300

    teste

--- src/boardgame/Peca.java
+++ src/boardgame/Peca.java
@@ -1,5 +1,17 @@
 package boardgame;
 
 public class Peca {
+	
+	protected Posicao posicao;
+	private Tabuleiro tabuleiro;
+	
+	public Peca(Tabuleiro tabuleiro) {
+		super();
+		this.tabuleiro = tabuleiro;
+	}
+
+	protected Tabuleiro getTabuleiro() {
+		return tabuleiro;
+	}
 
 }
---
+++ src/boardgame/Tabuleiro.java
@@ -0,0 +1,34 @@
+package boardgame;
+
+public class Tabuleiro {
+	
+	private int linha;
+	private int coluna;
+	private Peca[][] pecas;
+	
+	public Tabuleiro(int linha, int coluna) {
+		super();
+		this.setLinha(linha);
+		this.setColuna(coluna);
+		pecas = new Peca[linha][coluna];
+	}
+
+	public int getLinha() {
+		return linha;
+	}
+
+	public void setLinha(int linha) {
+		this.linha = linha;
+	}
+
+	public int getColuna() {
+		return coluna;
+	}
+
+	public void setColuna(int coluna) {
+		this.coluna = coluna;
+	}
+	
+	
+
+}
